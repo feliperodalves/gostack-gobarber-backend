9439e9076021d757bc28bda85871dc6bc622af0c
Felipe Alves <felipe.rod.alves@gmail.com>
2020-04-15 21:35:00 -0300

    feature: Adding repository & service for Appointment - Adicionado Repositorio - Adicionado Service de Criação de Appointment - Remodelado arquivo de rotas

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,7 +1,35 @@
 import { Router } from 'express';
+import { parseISO } from 'date-fns';
+import AppointmentsRepository from '../repositories/AppointmentsRepository';
+import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
+const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.post('/');
+appointmentsRouter.post('/', (req, res) => {
+  try {
+    const { provider, date } = req.body;
+
+    const parsedDate = parseISO(date);
+
+    const createAppointment = new CreateAppointmentService(
+      appointmentsRepository,
+    );
+    const appointment = createAppointment.execute({
+      provider,
+      date: parsedDate,
+    });
+
+    return res.json(appointment);
+  } catch (err) {
+    return res.status(400).json({ error: err.message });
+  }
+});
+
+appointmentsRouter.get('/', (req, res) => {
+  const appointments = appointmentsRepository.all();
+
+  return res.json(appointments);
+});
 
 export default appointmentsRouter;
