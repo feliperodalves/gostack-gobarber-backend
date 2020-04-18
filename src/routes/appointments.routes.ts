fd6ca5a008872bcfb0557fe4653f44b65f061518
Felipe Alves <felipe.rod.alves@gmail.com>
2020-04-18 13:01:37 -0300

    feature: Initial Database Config & Migrations/Models - Adicionado typeorm para gerenciar banco de dados - Criado configuração de banco postgres - Criado Migration e Model de Appointments - Alterado Rotas Repositories e Services para trabalhar com Banco - Incluido algumas regras no eslint e tsconfig

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,21 +1,20 @@
 import { Router } from 'express';
 import { parseISO } from 'date-fns';
+import { getCustomRepository } from 'typeorm';
+
 import AppointmentsRepository from '../repositories/AppointmentsRepository';
 import CreateAppointmentService from '../services/CreateAppointmentService';
 
 const appointmentsRouter = Router();
-const appointmentsRepository = new AppointmentsRepository();
 
-appointmentsRouter.post('/', (req, res) => {
+appointmentsRouter.post('/', async (req, res) => {
   try {
     const { provider, date } = req.body;
 
     const parsedDate = parseISO(date);
 
-    const createAppointment = new CreateAppointmentService(
-      appointmentsRepository,
-    );
-    const appointment = createAppointment.execute({
+    const createAppointment = new CreateAppointmentService();
+    const appointment = await createAppointment.execute({
       provider,
       date: parsedDate,
     });
@@ -26,8 +25,9 @@ appointmentsRouter.post('/', (req, res) => {
   }
 });
 
-appointmentsRouter.get('/', (req, res) => {
-  const appointments = appointmentsRepository.all();
+appointmentsRouter.get('/', async (req, res) => {
+  const appointmentsRepository = getCustomRepository(AppointmentsRepository);
+  const appointments = await appointmentsRepository.find();
 
   return res.json(appointments);
 });
