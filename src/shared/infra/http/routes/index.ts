75a7ab90c6f8329915d881b115c9391697f6df10
Felipe Alves <felipe.rod.alves@gmail.com>
2020-05-20 17:55:53 -0300

    feature: Profile Controller & Services

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -3,6 +3,7 @@ import appointmentRouter from '@modules/appointments/infra/http/routes/appointme
 import usersRouters from '@modules/users/infra/http/routes/user.routes';
 import sessionsRouter from '@modules/users/infra/http/routes/session.routes';
 import passwordRouter from '@modules/users/infra/http/routes/password.routes';
+import profileRouter from '@modules/users/infra/http/routes/profiles.routes';
 
 const routes = Router();
 
@@ -10,5 +11,6 @@ routes.use('/appointments', appointmentRouter);
 routes.use('/users', usersRouters);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
+routes.use('/profile', profileRouter);
 
 export default routes;
