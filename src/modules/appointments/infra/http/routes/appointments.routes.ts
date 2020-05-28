dae91059d5c4659d4b0a827fe03aa886e448d6ef
Felipe Alves <felipe.rod.alves@gmail.com>
2020-05-28 13:25:39 -0300

    feature: fix usertoken repository

--- src/modules/appointments/infra/http/routes/appointments.routes.ts
+++ src/modules/appointments/infra/http/routes/appointments.routes.ts
@@ -23,11 +23,4 @@ appointmentsRouter.post(
 );
 appointmentsRouter.get('/me', providerAppointmentsController.index);
 
-// appointmentsRouter.get('/', async (req, res) => {
-//   const appointmentsRepository = container.resolve(AppointmentsRepository);
-//   const appointments = await appointmentsRepository.find();
-
-//   return res.json(appointments);
-// });
-
 export default appointmentsRouter;
